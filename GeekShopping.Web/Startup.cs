e90ee79dd4a4fb8539db4938fd8969bb408393c5
Roberto Frisanco dos Santos <roberto.frisanco@univag.iemat>
2022-12-27 14:14:28 -0400

    starting the layout product

--- GeekShopping.Web/Startup.cs
+++ GeekShopping.Web/Startup.cs
@@ -24,8 +24,8 @@ namespace GeekShopping.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient<IProductService, ProductService>(
-                c => c.BaseAddress = new Uri(Configuration["ServiceUrls:ProductAPI"])
+            services.AddHttpClient<IProductService, ProductService>(c =>
+                    c.BaseAddress = new Uri(Configuration["ServicesUrls:ProductAPI"])
                 );
             services.AddControllersWithViews();
         }
